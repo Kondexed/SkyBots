cc82bec2b50a4a045363752037ee52aa3ec5d92e
Vitorsis <42366267+Vitorsis@users.noreply.github.com>
2018-08-20 10:31:08 +1000

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@
      ] //filter
  
      let filterBypass = [
-         '428763114251616256, 476643015511900181, 346890172174041099'
+         ' 476643015511900181'
      ] //These ID's can bypass filter.
  
      let i = 0;
